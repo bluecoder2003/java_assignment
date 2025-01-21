f13255791250a7abdd02d2491c518705458d7ea1
bluecoder2003 <130232862+bluecoder2003@users.noreply.github.com>
2025-01-21 09:12:25 +0530

    happy number fixed

--- assignment_7/happy.java
+++ assignment_7/happy.java
@@ -1,25 +1,54 @@
+package assignment_7;
 import java.util.Scanner;
 
 public class happy {
-    public static void main(String args[]) {
+
+    // Method to compute the sum of the squares of the digits of a number
+    static int numSquareSum(int n) {
+        int squareSum = 0;
+        while (n != 0) {
+            squareSum += (n % 10) * (n % 10);  // Square the last digit and add to squareSum
+            n /= 10;                            // Remove the last digit
+        }
+        return squareSum;  // Return the computed sum
+    }
+
+    // Method to check if a number is a Happy number
+    static boolean isHappynumber(int n) {
+        int slow, fast;
+
+        // Initialize slow and fast pointers
+        slow = fast = n;
+        do {
+            // Move slow number by one iteration
+            slow = numSquareSum(slow);
+
+            // Move fast number by two iterations
+            fast = numSquareSum(numSquareSum(fast));
+
+        } while (slow != fast);
+
+        // If both meet at 1, then it is a Happy number
+        return (slow == 1);
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string");
-        String n = sc.nextLine().toUpperCase();//use nextline to read the whole line and use next to read only one word
-        int l=n.length();
-        int value=0;
-        for (int i = 0; i < l; i++) {
-            char ch = n.charAt(i);
-            if (ch >= 'A' && ch <= 'Z') {
-                value = ch - 'A' + 1;
-            }
+        System.out.println("Enter a number:");
+        String s = sc.nextLine().toUpperCase(); 
+        char[] ch=s.toCharArray();
+        int pos=0;
+        String concatenated="";
+        for(char c: ch)
+        {
+            pos =c-'A'+1;
+            concatenated+=pos;
         }
-        int temp=value;
-        int sum=0;
-        while(temp>0){
-            int rem=temp%10;
-            sum+=rem;
-            temp/=10;
-        }
-        sc.close();
+        int n = Integer.parseInt(concatenated);
+
+        if (isHappynumber(n))
+            System.out.println(n + " is a Happy number");
+        else
+            System.out.println(n + " is not a Happy number");
     }
 }
